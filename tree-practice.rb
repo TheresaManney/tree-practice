4f9ec4a31b141cc2a8440427fc4be18c70796168
Theresa Manney <theresa.m.17@gmail.com>
2017-05-11 22:48:10 -0700

    Completed inclass exercise

--- tree-practice.rb
+++ tree-practice.rb
@@ -15,7 +15,25 @@ def print_infix(node)
   print_infix(node.right)
 end
 
-root = TreeNode.new("+")
-root.left = TreeNode.new("3")
-root.right = TreeNode.new("2")
+# Ex 1:
+# root = TreeNode.new("+")
+# root.left = TreeNode.new("3")
+# root.right = TreeNode.new("2")
+# Can change these things
+# Ex 2:
+# root = TreeNode.new("+")
+# root.left = TreeNode.new("3")
+# root.right = TreeNode.new("-")
+# root.right.left = TreeNode.new("2")
+# root.right.right = TreeNode.new("10")
+# Ex 3:
+root = TreeNode.new("-")
+root.left = TreeNode.new("+")
+root.left.left = TreeNode.new("*")
+root.left.left.left = TreeNode.new("4")
+root.left.left.right = TreeNode.new("3")
+root.left.right = TreeNode.new("2")
+root.right = TreeNode.new("%")
+root.right.left = TreeNode.new("10")
+root.right.right = TreeNode.new("5")
 print_infix(root)
